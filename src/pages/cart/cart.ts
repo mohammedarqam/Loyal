7468cbb24335893d1a236caecefcc1e4bc172f79
mohammedarqam <mohammedarqam97@gmail.com>
2018-08-19 00:58:09 +0530

    Orders Added

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, App, LoadingController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, App, LoadingController, AlertController } from 'ionic-angular';
 import * as firebase from 'firebase';
 import { LoginPage } from '../login/login';
-
+import moment from 'moment';
 
 
 @IonicPage()
@@ -17,10 +17,12 @@ export class CartPage {
   public cartItems : Array<any> = [];
   data : boolean = false;
   totCartValue : number=0;
-
+  creditAvailable : number = 0;
+  
   constructor(
   public navCtrl: NavController, 
   public app : App,
+  public alertCtrl : AlertController,
   public loadingCtrl : LoadingController, 
   public navParams: NavParams) {
     firebase.auth().onAuthStateChanged((user) => {
@@ -30,12 +32,14 @@ export class CartPage {
       }
     });
     this.getCart();
-    this.getCartValue()
+    this.getCartValue();
+    this.getCreditVal();
   }
 
   ionViewDidEnter(){
     this.getCart();
-    this.getCartValue()
+    this.getCartValue();
+    this.getCreditVal();
   }
 
   getCart(){
@@ -85,16 +89,73 @@ export class CartPage {
   
 }
 
-  order(){
+getCreditVal(){
+  firebase.database().ref("Users/").child(firebase.auth().currentUser.uid).once("value",creditVal=>{
+    var CreditLimit = creditVal.val().CreditLimit;
+    var CreditUsed = creditVal.val().CreditUsed;
+    this.creditAvailable = CreditLimit - CreditUsed;
+  })
+}
+
+
+  creditPermission(){
+    let alert = this.alertCtrl.create({
+      title: 'Use Credit Balance ?',
+      message :"You have "+ this.creditAvailable+ " available in limit.",
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: data => {
+            this.order("0")
+          }
+        },
+        {
+          text: 'Use',
+          handler: data => {
+            this.order(this.creditAvailable)
+          }
+        }
+      ]
+    });
+    alert.present();
+
+  }
+
+
+
+  order(credit){
     let loading = this.loadingCtrl.create({
       content: 'Please wait...'
     });
     loading.present();
 
+    firebase.database().ref("Orders/").push({
+      User : firebase.auth().currentUser.uid,
+      Items : this.cartItems,
+      Value : this.totCartValue,
+      TimeStamp : moment().format(),
+      Status : "Pending",
+      CreditUsed : credit,
+      OrderId : this.genOrderId()
+    }).then(()=>{
+      this.getCart();
+      this.getCartValue();
       loading.dismiss();
+
+    })
+
   }
 
-
+  genOrderId(){
+    var length = 12,
+        charset = "0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+  }
 
 
 }
